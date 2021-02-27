cf616906d5bf5a5b1fea523681e37f84bf4065f5
chintul <38564703+chintul@users.noreply.github.com>
2021-02-27 15:53:33 +0800

    sad

--- test/src/test/FibonacciGenerator.java
+++ test/src/test/FibonacciGenerator.java
@@ -1,10 +1,45 @@
 package test;
 
+/**
+ * A FibonacciGenerator generates a stream of Fibonacci numbers
+ * that can be traversed forwards or backwards.
+ */
+
 public class FibonacciGenerator {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private int first = 0; // current Fib number
+	private int second = 1; // next Fib number
+	
+	/** 
+	 * Returns the current Fibonacci number in this generator and 
+	 * updates the stream to the next Fibonacci number.
+	 * @return the current Fibonacci number
+	 */
+	public int next() {
+		int oldFirst = first;
+		
+		first = second;
+		second += oldFirst;
+		
+		return oldFirst;
 	}
 
+	/**
+	 * Returns the previous Fibonacci number in this generator and 
+	 * updates the stream to that number. 
+	 * @return the previous Fibonacci number
+	 */
+	public int previous() {
+		
+		if (first == 0) {
+			throw new IllegalStateException("generator is already at the start of the fibonacci sequence");
+		}
+		
+		int oldFirst = first;
+		
+		first = second - first;
+		second = oldFirst;
+			
+		return first;
+	}
 }
--- test/src/test/FibonacciTest.java
+++ test/src/test/FibonacciTest.java
@@ -1,10 +1,64 @@
 package test;
 
-public class FibonacciTest {
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class FibonacciTest {
+	
+	// An array containing the beginning of the Fibonacci sequence.
+	private static final int[] fibNumbers = { 
+			0, 1, 1, 2, 3, 5, 8, 13, 
+			21, 34, 55, 89, 144, 233, 
+			377, 610, 987, 1597, 2584, 
+			4181, 6765, 10946, 17711, 28657, 
+			46368, 75025, 121393, 196418, 317811 };
+	
+	// This is the FibonacciGenerator that we will use in all of our tests.
+	private FibonacciGenerator fibGenerator;
+	
+	// A method that is called before the invocation of any test method.
+	@Before public void makeNewGenerator() {
+		fibGenerator = new FibonacciGenerator();
+	}
+	
+	// A test method.
+	@Test public void generateAndCheck() {
+		
+		// This for-loop takes each int in fibNumbers, sets the value of i to
+		// that int, and then executes the code within the braces.
+		for (int i : fibNumbers) {
+			Assert.assertEquals(i, fibGenerator.next());
+		}
+		
+		// This for-loop loops backwords from the last int in fibNumbers to the
+		// first.
+		for (int i = fibNumbers.length - 1; i >= 0; i--) {
+			Assert.assertEquals(fibNumbers[i], fibGenerator.previous());
+		}
+		
+		// We repeat the first for-loop, to ensure that our calls to prev have 
+		// returned the generator to the beginning of the Fibonacci sequence.
+		for (int number : fibNumbers) {
+			Assert.assertEquals(number, fibGenerator.next());
+		}
 
 	}
-
+	
+	// Another test method.
+	@Test(expected=IllegalStateException.class)
+	public void callIllegalPrevious() {
+		
+		// Calling prev here should result in an exception, because the 
+		// first number returned by a FibonacciGenerator is 0, and there is 
+		// no Fibonacci before 0.	
+		fibGenerator.previous();
+	}
+	
+	// A method that is called after the invocation of every test method.
+	@After public void nullGenerator() {
+		fibGenerator = null;
+	}
+	
 }
