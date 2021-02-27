34dc9fd194a7e1097a2b500d376c9269f4598b04
chintul <38564703+chintul@users.noreply.github.com>
2021-02-27 15:53:02 +0800

    fibtest

---
+++ test/src/test/FibonacciGenerator.java
@@ -0,0 +1,10 @@
+package test;
+
+public class FibonacciGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ test/src/test/FibonacciTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class FibonacciTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
